7c3ba4624d10916a1eebf26d090c21b95088e25b
mayankgorana <mayankgorana2003@gmail.com>
2024-02-07 18:21:10 +0530

    forth

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ const Port = 3000;
 app.use(express.static('public'));
 
 app.get('/' , (req,res)=>{
-    res.render("index.ejs")
-})
+    res.render("index.ejs");
+});
+
+app.get('/services' , (req,res)=>{
+    res.render("services.ejs");
+});
+
+app.get('/plans' , (req,res)=>{
+    res.render("plans.ejs");
+});
+
+app.get('/team' , (req,res)=>{
+    res.render("team.ejs");
+});
+
+
+app.listen(Port, ()=>{
+    console.log(`Server running on port ${Port}`);
+});
